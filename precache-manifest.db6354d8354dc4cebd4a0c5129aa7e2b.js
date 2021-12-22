797d48c4689d9e32f824a7c9ae0a74bba6377040
lab_1_10 <lab_1_10@lab-1-10s-iMac.local>
2021-12-22 11:29:55 +0800

    Updates

---
+++ precache-manifest.db6354d8354dc4cebd4a0c5129aa7e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fa29b0026e62df1c62d",
+    "url": "/NBayanbat/course-directory-new-new/static/css/main.5a5a04ff.chunk.css"
+  },
+  {
+    "revision": "9fa29b0026e62df1c62d",
+    "url": "/NBayanbat/course-directory-new-new/static/js/main.9fa29b00.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/NBayanbat/course-directory-new-new/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "a3a386d4339625d09994",
+    "url": "/NBayanbat/course-directory-new-new/static/js/runtime~main.a3a386d4.js"
+  },
+  {
+    "revision": "dac9b964ebc4b46150940f057dd41bf6",
+    "url": "/NBayanbat/course-directory-new-new/index.html"
+  }
+];
